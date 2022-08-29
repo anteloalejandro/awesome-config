b315e9f523f26f28e673e334b6197b30d0a43023
Alejandro Antelo <alfasant@protonmail.com>
2022-08-29 14:10:56 +0200

    Completed 'main' directory

---
+++ main/menu.lua
@@ -0,0 +1,25 @@
+local awful = require("awful")
+local menubar = require("menubar")
+local beautiful = require("beautiful")
+local apps = require("main.user-variables").apps
+
+local terminal = apps.terminal
+local editor = apps.editor
+local editor_cmd = terminal .. " -e " .. editor
+
+local myawesomemenu = {
+
+  { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
+  { "manual", terminal .. " -e man awesome" },
+  { "edit config", editor_cmd .. " " .. awesome.conffile },
+  { "restart", awesome.restart },
+  { "quit", function() awesome.quit() end },
+}
+
+mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon }, { "open terminal", terminal } } })
+
+mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
+menu = mymainmenu })
+
+-- Menubar configuration
+menubar.utils.terminal = terminal -- Set the terminal for applications that require it
---
+++ main/user-variables.lua
@@ -0,0 +1,14 @@
+return {
+
+  apps = {
+    terminal="alacritty",
+    editor="nvim",
+    browser="firefox",
+    file_manager="pcmanfm"
+  },
+
+  modkey = "Mod4",
+
+  wallpaper="./wallpapers/dark-material.jpg"
+
+}
